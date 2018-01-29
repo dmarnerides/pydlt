623f1883ac096c6bb65964dffc3986ca845a65e9
Demetris Marnerides <dmarnerides@gmail.com>
2018-01-29 19:20:25 +0000

    Changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 from setuptools import setup, find_packages
 
-version = '0.0.1'
+version = '0.0.2'
 
 # Got this from here
 # http://blogs.nopcode.org/brainstorm/2013/05/20/pragmatic-python-versioning-via-setuptools-and-git-tags/
