4b132448c8cd565f61a80e6db0778247f31b35b9
Demetris Marnerides <dmarnerides@gmail.com>
2018-02-01 16:10:31 +0000

    where am I?

--- conda.recipe/conda_upload.sh
+++ conda.recipe/conda_upload.sh
@@ -4,6 +4,8 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export GIT_BUILD_STR=$(git describe --always)
-export VERSION=$(cat ../.version_str)
+echo "Trying to set version"
+echo $(pwd)
+export VERSION=$(cat .version_str)
 conda build .
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u demetris -l nightly $CONDA_BLD_PATH/$OS/pydlt-$VERSION-0.tar.bz2 --force
