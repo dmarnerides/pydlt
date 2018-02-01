b3c8b47f32ad935fb6b312016dca1dc4222e5102
Demetris Marnerides <dmarnerides@gmail.com>
2018-02-01 16:17:07 +0000

    no test and correct directory

--- conda.recipe/conda_upload.sh
+++ conda.recipe/conda_upload.sh
@@ -4,8 +4,6 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export GIT_BUILD_STR=$(git describe --always)
-echo "Trying to set version"
-echo $(pwd)
 export VERSION=$(cat .version_str)
-conda build .
+conda build conda.recipe --no-test
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u demetris -l nightly $CONDA_BLD_PATH/$OS/pydlt-$VERSION-0.tar.bz2 --force
