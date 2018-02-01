418b22252618f6533f21e3481713254a6dfbcffc
Demetris Marnerides <dmarnerides@gmail.com>
2018-02-01 16:28:32 +0000

    this should do it

--- conda.recipe/conda_upload.sh
+++ conda.recipe/conda_upload.sh
@@ -5,4 +5,4 @@ export CONDA_BLD_PATH=~/conda-bld
 export GIT_BUILD_STR=$(git describe --always)
 export VERSION=$(cat .version_str)
 conda build conda.recipe --no-test
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u demetris -l nightly $CONDA_BLD_PATH/noarch/pydlt-$VERSION-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u demetris -l nightly $CONDA_BLD_PATH/noarch/pydlt-$VERSION-$GIT_BUILD_STR.tar.bz2 --force
