1c2c3a876cef1649fa4d9b222bd631a6f5529669
Demetris Marnerides <dmarnerides@gmail.com>
2018-02-01 16:21:00 +0000

    oh yes it's noarch

--- conda.recipe/conda_upload.sh
+++ conda.recipe/conda_upload.sh
@@ -1,9 +1,8 @@
 
-OS=$TRAVIS_OS_NAME-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export GIT_BUILD_STR=$(git describe --always)
 export VERSION=$(cat .version_str)
 conda build conda.recipe --no-test
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u demetris -l nightly $CONDA_BLD_PATH/$OS/pydlt-$VERSION-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u demetris -l nightly $CONDA_BLD_PATH/noarch/pydlt-$VERSION-0.tar.bz2 --force
