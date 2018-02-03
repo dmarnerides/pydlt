76a0fef7acf24b9cb3710a3c76e006b006ab7f2d
Demetris Marnerides <dmarnerides@gmail.com>
2018-02-03 12:41:32 +0000

    removed nightly

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -5,4 +5,4 @@ export CONDA_BLD_PATH=~/conda-bld
 export GIT_BUILD_STR=$(git describe --always)
 export VERSION=$(cat .version)
 conda build conda --no-test
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u demetris -l nightly $CONDA_BLD_PATH/noarch/pydlt-$VERSION-$GIT_BUILD_STR.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u demetris $CONDA_BLD_PATH/noarch/pydlt-$VERSION-$GIT_BUILD_STR.tar.bz2 --force
