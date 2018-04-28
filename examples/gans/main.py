fa63c13d1afa1e678d634e859f1907596839b9f1
Demetris Marnerides <dmarnerides@gmail.com>
2018-04-28 16:52:44 +0100

    Updated examples for new version

--- examples/gans/main.py
+++ examples/gans/main.py
@@ -2,6 +2,7 @@ import torch
 import dlt
 from dlt.train import VanillaGANTrainer as GAN
 from dlt.train import WGANGPTrainer as WGANGP
+from dlt.train import WGANCTTrainer as WGANCT
 from dlt.train import BEGANTrainer as BEGAN
 from dlt.train import FisherGANTrainer as FisherGAN
 from models import *
@@ -10,25 +11,27 @@ from models import *
 dlt.config.make_subsets({'model': ['generator', 'discriminator'],
                          'optimizer': ['generator', 'discriminator']})
 dlt.config.add_extras([
-    dict(flag='--gan_type', choices=['vanilla', 'wgan-gp', 'began', 'fishergan'], default='vanilla', help='Gan type'),
+    dict(flag='--gan_type', choices=['vanilla', 'wgan-gp', 'wgan-ct', 'began', 'fishergan'],
+                            default='vanilla', help='Gan type'),
     dict(flag='--num_hidden', type=int, default=64, help='Number of hidden units'),
     dict(flag='--z_dim', type=int, default=128, help='Input noise dimensionality'),
     dict(flag='--lambda_gp', type=float, default=10, help='Gradient penalty magnitude'),
+    dict(flag='--m_ct', type=float, default=0.001, help='Constant bound for consistency term for WGAN-CT'),
+    dict(flag='--lambda_ct', type=float, default=0.001, help='Weight of consistency term for WGAN-CT'),
     dict(flag='--lambda_k', type=float, default=0.001, help='Learning rate for k for BEGAN'),
     dict(flag='--gamma', type=float, default=0.5, help='Gamma for BEGAN (diversity ratio)'),
     dict(flag='--rho', type=float, default=1e-6, help='rho for FisherGAN'),
-    dict(flag='--d_iter', type=int, default=2, help='Number of discriminator steps per generator'),
-    dict(flag='--show_progress', type=dlt.util.str2bool, default=True, help='Show samples while training'),
+    dict(flag='--d_iter', type=int, default=2, help='Number of discriminator steps per generator')
 ])
-opt = dlt.config.parse()
+opt = dlt.config.parse(verbose=True)
 
 # Configure seeds
 if opt.seed is not None:
     torch.manual_seed(opt.seed)
 
 # Data
-sizes = {'mnist': (1,28), 'fashionmnist': (1,28),
-         'cifar10': (3,32), 'cifar100': (3,32)}
+sizes = {'mnist': (1, 28), 'fashionmnist': (1, 28),
+         'cifar10': (3, 32), 'cifar100': (3, 32)}
 if opt.torchvision_dataset not in sizes:
     raise ValueError('--torchvision_dataset must be one of {0}'.format(','.join(sizes.keys())))
 size = sizes[opt.torchvision_dataset]
@@ -45,6 +48,7 @@ loader = dlt.config.loader(dataset, preprocess)
 # Models
 generator = Generator(opt.num_hidden, opt.z_dim, size[0], size[1])
 gen_chkp = dlt.config.model_checkpointer(generator, subset='generator')
+
 if opt.gan_type == 'began':
     discriminator = DiscriminatorBEGAN(opt.num_hidden, size[0], size[1])    
 else:
@@ -59,8 +63,10 @@ if opt.use_gpu:
     discriminator.cuda()
 
 # Optimizers
-g_optim, g_optim_chkp = dlt.config.optimizer(generator, subset='generator')
-d_optim, d_optim_chkp = dlt.config.optimizer(discriminator, subset='discriminator')
+g_optim = dlt.config.optimizer(generator, subset='generator')
+g_optim_chkp = dlt.config.optimizer_checkpointer(g_optim, subset='generator')
+d_optim = dlt.config.optimizer(discriminator, subset='discriminator')
+d_optim_chkp = dlt.config.optimizer_checkpointer(d_optim, subset='discriminator')
 
 # Trainer
 if opt.gan_type == 'wgan-gp':
@@ -69,34 +75,37 @@ elif opt.gan_type == 'began':
     trainer = BEGAN(generator, discriminator, g_optim, d_optim, opt.lambda_k, opt.gamma, opt.d_iter)
 elif opt.gan_type == 'fishergan':
     trainer = FisherGAN(generator, discriminator, g_optim, d_optim, opt.rho, opt.d_iter)
+elif opt.gan_type == 'wgan-ct':
+    trainer = WGANCT(generator, discriminator, g_optim, d_optim, opt.lambda_gp, opt.m_ct, opt.lambda_ct, opt.d_iter)
 else:
     trainer = GAN(generator, discriminator, g_optim, d_optim, opt.d_iter)
+
+trainer_chkp = dlt.config.trainer_checkpointer(trainer)
 
 if opt.use_gpu:
     trainer.cuda() # Trainers might have buffers that need to be transferred to GPU
 
 # Logging
 log = dlt.util.Logger('training', trainer.loss_names_training(), opt.save_path)
-# epoch checkpoint
-epoch_chkp, current_epoch = dlt.config.epoch_checkpointer()
-
 
 # Training loop
-for epoch in range(current_epoch, opt.max_epochs):
-    print('-'*79 + '\nEpoch {0}:'.format(epoch))
-    trainer.train()
-    for i, (batch, (prediction, losses)) in enumerate(trainer(loader)):
-        # Show progress
-        if opt.show_progress and i % 100 == 0:
-            imgs = [dlt.util.map_range(x) for x in [batch[1], prediction]]
-            img_grid = dlt.util.make_grid(imgs, color=size[0] == 3)
-            dlt.viz.imshow(img_grid, pause=0.02, title='Epoch {0}, Iteration {1}'.format(epoch, i+1))
-        # Log the losses
-        log(losses['d_loss'])
-    # Do some checkpointomg
-    epoch_chkp(epoch + 1) # +1 because this epoch has finished
+for epoch in range(trainer.epoch, opt.max_epochs):
     tag = 'epoch-{0}'.format(epoch)
+    print('-'*79 + '\nEpoch {0}:'.format(epoch))
+    # Set to training mode
+    trainer.train()
+    # The trainer iterator performs the optimization and gives predictions and
+    # losses at each iteration
+    for i, (batch, (prediction, losses)) in enumerate(trainer(loader)):
+        # Show progress of each iteration and log the losses
+        dlt.config.sample_images([batch[1], prediction], color=size[0] == 3,
+                                 preprocess=dlt.util.map_range, tag=tag)
+        log(losses)
+    
+    # Checkpoint everything
     gen_chkp(generator, tag=tag)
     disc_chkp(discriminator, tag=tag)
     g_optim_chkp(g_optim, tag=tag)
     d_optim_chkp(d_optim, tag=tag)
+    trainer_chkp(trainer, tag=tag)
+    
--- examples/gans/models.py
+++ examples/gans/models.py
@@ -1,12 +1,11 @@
-import math
 from torch import nn
 
 def selu_init(model):
     for m in model.modules():
-        if hasattr(m,'pretrained') and m.pretrained:
-            continue
         if any([isinstance(m, x) for x in [nn.Conv2d, nn.ConvTranspose2d, nn.Linear]]):
-            nn.init.kaiming_normal_(m.weight.data, 1)
+            nn.init.kaiming_normal_(m.weight, 1)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, val=0)
 
 class Generator(nn.Module):
     def __init__(self, num_hidden, z_dim, num_chan, num_pix):
@@ -35,13 +34,19 @@ class Discriminator(nn.Module):
             nn.Linear(num_chan*num_pix*num_pix, num_hidden),
             nn.SELU(),
             nn.Linear(num_hidden, num_hidden),
-            nn.SELU(),
-            nn.Linear(num_hidden, 1),
+            nn.SELU()
         )
+        self.last_layer = nn.Linear(num_hidden, 1)
         selu_init(self)
 
-    def forward(self, v_input):
-        return self.main(v_input.view(v_input.size(0), -1))
+    # The correction term is for WGAN-CT
+    def forward(self, v_input, correction_term=False):
+        if correction_term:
+            main = self.main(v_input.view(v_input.size(0), -1))
+            noisy_main = nn.functional.dropout(main, p=0.1)
+            return main, self.last_layer(noisy_main)
+        else:
+            return self.last_layer(self.main(v_input.view(v_input.size(0), -1)))
 
 # BEGAN needs an autoencoding discriminator
 class DiscriminatorBEGAN(nn.Module):
