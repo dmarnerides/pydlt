b17049afbd85658bfd5172882c9ed3eb73efefa8
Demetris Marnerides <dmarnerides@gmail.com>
2018-04-30 18:31:45 +0100

    Added partial losses

--- dlt/train/vanilla.py
+++ dlt/train/vanilla.py
@@ -13,7 +13,8 @@ class VanillaTrainer(BaseTrainer):
     Each iteration returns the mini-batch and a tuple containing:
 
         - The model prediction.
-        - A dictionary with the `training_loss` or `validation_loss`.
+        - A dictionary with the `training_loss` or `validation_loss`
+          (along with the partial losses, if criterion returns a dictionary).
     
     Example:
         >>> trainer = dlt.train.VanillaTrainer(my_model, nn.L1Loss(), my_optimizer)
@@ -25,6 +26,11 @@ class VanillaTrainer(BaseTrainer):
         >>> trainer.eval()
         >>> for batch, (prediction, loss) in trainer(valid_data_loader):
         >>>     print(loss['validation_loss'])
+
+    Note:
+        If the criterion returns a dict of (named) losses, then they are added
+        together to backpropagate. The total is returned along with all the
+        partial losses.
     """
     def __init__(self, model, criterion, optimizer):
         super(VanillaTrainer, self).__init__()
@@ -36,14 +42,29 @@ class VanillaTrainer(BaseTrainer):
         self._losses['validation'] = ['validation_loss']
 
         self.criterion = criterion
+        if hasattr(criterion, 'loss_list'):
+            for loss in criterion.loss_list:
+                self._losses['training'] += [loss]
+                self._losses['validation'] += [loss]
+
 
     def iteration(self, data):
-
+        loss_key = 'training_loss' if self.training else 'validation_loss'
         v_pred = self._models['model'](data[0])
-        loss = self.criterion(v_pred, data[1])
+        losses = self.criterion(v_pred, data[1])
+        if isinstance(losses, dict):
+            total_loss = 0
+            for key, val in losses.items():
+                total_loss = total_loss + val
+                losses[key] = val.item()
+            losses[loss_key] = total_loss.item()
+        else:
+            total_loss = losses
+            losses = {loss_key: total_loss.item()}
+
         if self.training:
             self._optimizers['optimizer'].zero_grad()
-            loss.backward()
+            total_loss.backward()
             self._optimizers['optimizer'].step()
-        key = 'training_loss' if self.training else 'validation_loss'
-        return v_pred, {key: loss.item()}
+        
+        return v_pred, losses
